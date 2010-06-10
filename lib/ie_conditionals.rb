0bfca96bdd6d9fd1b6db837d73ddf95a6d76ea2e
Nathan Bertram <nbertram@gmail.com>
2010-06-09 19:57:40 -0500

    one less line of code

--- lib/ie_conditionals.rb
+++ lib/ie_conditionals.rb
@@ -6,15 +6,14 @@ class IEConditionals
       opts[:only] = opts[:only].map(&:to_i) if opts[:only]
       prefix = opts[:prefix] || 'ie'
       versions = opts[:only] || [6,7,8]
+      output = ''
       if block_given?
-        output = ''
         output += "<!--[if IE 8]><div id=\"#{prefix}8\"><![endif]-->\n" if versions.include?(8)
         output += "<!--[if IE 7]><div id=\"#{prefix}7\"><![endif]-->\n" if versions.include?(7)
         output += "<!--[if IE 6]><div id=\"#{prefix}6\"><![endif]-->\n" if versions.include?(6)
         output += capture(&block)
         output += "<!--[if IE]></div><![endif]-->" unless versions.blank?
       else
-        output = ''
         output += "<!--[if IE 8]>#{stylesheet_link_tag("#{prefix}8")}<![endif]-->\n" if versions.include?(8)
         output += "<!--[if IE 7]>#{stylesheet_link_tag("#{prefix}7")}<![endif]-->\n" if versions.include?(7)
         output += "<!--[if IE 6]>#{stylesheet_link_tag("#{prefix}6")}<![endif]-->\n" if versions.include?(6)
